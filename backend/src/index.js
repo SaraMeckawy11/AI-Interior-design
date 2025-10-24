6862fbd8db63129caf608679866fb00c155c2932
SaraMeckawy11 <sara123meckawy@gmail.com>
2025-10-24 12:27:19 +0300

    try

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,9 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-job.start();
+// job.start();
 app.use(express.json({ limit: '100mb' }));
-//app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
